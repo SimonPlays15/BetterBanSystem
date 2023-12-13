1fb62e13adc62d128c9c7540514a2ab64237c1be
Simon W. <58950483+SimonPlays15@users.noreply.github.com>
2023-12-13 15:12:01 +0100

    Resolved Spigot Dependencies

---
+++ src/main/java/we/github/mcdevstudios/betterbansystem/BetterBanSystem.java
@@ -0,0 +1,11 @@
+package we.github.mcdevstudios.betterbansystem;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BetterBanSystem extends JavaPlugin {
+
+    public void onEnable(){
+        // Start method
+    }
+
+}
--- src/main/java/we/github/mcdevstudios/betterbansystem/Main.java
+++
@@ -1,7 +0,0 @@
-package we.github.mcdevstudios.betterbansystem;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
